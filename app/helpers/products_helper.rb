425ed1da02f628979bb6a6e5933fa4677a252e64
bruceqiao <bruceq619@gmail.com>
2017-03-02 09:29:37 +0800

    add discount_percentage to show page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,11 @@
 module ProductsHelper
 
-  def render_product_description(product) 
+  def render_product_description(product)
     simple_format(product.description)
+  end
+
+  def discount_percentage(product)
+    (product.taoprice - product.price)*100/product.taoprice
   end
 
 end
